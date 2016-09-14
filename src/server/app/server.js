c1f14f292b41cfc325fe4649be621ee795176efc
LARABA MOHAMMED SEDDIK <lmseddik@icloud.com>
2016-09-14 19:26:56 +0100

    Added sqlite database

--- src/server/app/server.js
+++ src/server/app/server.js
@@ -1,51 +1,68 @@
-﻿
-// call the packages we need
-var express = require('express');        // call express
-var app = express();                 // define our app using express
-var bodyParser = require('body-parser');
+﻿var Sequelize = require('sequelize')
+var sqlite3 = require('sqlite3').verbose();
+var path = require('path');
+var dbPath = path.resolve(__dirname, 'touggourti.db')
+var sequelize = new Sequelize('', '', '', { dialect: 'sqlite', storage: dbPath });
+var db = new sqlite3.Database(dbPath);
 
-// configure app to use bodyParser()
-// this will let us get the data from a POST
-app.use(bodyParser.urlencoded({ extended: true }));
-app.use(bodyParser.json());
+db.serialize(function () {
+    db.run("CREATE TABLE IF NOT EXISTS [activite] (\
+[id] INTEGER  NOT NULL PRIMARY KEY AUTOINCREMENT,\
+[nom] TEXT  UNIQUE NOT NULL,\
+[ar_nom] TEXT  UNIQUE NOT NULL,\
+[domaine] NVARCHAR(50)  NULL,\
+[tel] NVARCHAR(50)  NULL,\
+[mobile] NVARCHAR(50)  NULL,\
+[categorie] NVARCHAR(50) DEFAULT 'All' NULL,\
+[keywords] TEXT  NULL,\
+[gps] NVARCHAR(50)  NULL,\
+[logo] TEXT  NULL,\
+[adresse] TEXT  NULL,\
+[email] NVARCHAR(50)  NULL,\
+[website] NVARCHAR(50)  NULL,\
+[fax] NVARCHAR(50)  NULL,\
+[description] TEXT  NULL\
+)");
 
-var port = process.env.PORT || 5213;        // set our port
-
-// ROUTES FOR OUR API
-// =============================================================================
-var router = express.Router();              // get an instance of the express Router
-
-// test route to make sure everything is working (accessed at GET http://localhost:5213/api)
-router.get('/', function (req, res) {
-    res.json({ message: 'touggourti' });
 });
-router.get('/activite', function (req, res, next) {
-    res.header('Access-Control-Allow-Origin', '*');
-    res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
-    res.header('Access-Control-Allow-Headers', 'Content-Type');
-    res.json({
-        "1": {
-            nom: 'Optim IT', acti: "Bureau d'études informatique", tel: "0540437705", categorie: "Informatique",
-            keywords: "boite de développement,logiciels,sites web, infographie"
-        },
-        "2": {
-            nom: 'Pharmacie Bassa', acti: "Pharmacie", tel: "029671312", categorie: "Medical",
-            keywords: "médicaments,pharmacie"
-        },
-        "3": {
-            nom: 'Moussaoui Informatique', acti: "Vente et maintenance matériel informatique", tel: "029667080", categorie: "Informatique",
-            keywords: "desktop,laptop,matériel,informatique,code a barre,maintenance"
+
+
+var express = require('express');
+var restapi = express();
+
+restapi.get('/api', function (req, res) {
+    res.json({ "message": "hello" });
+
+});
+
+restapi.get('/api/activite', function (req, res) {
+
+
+    db.all("SELECT * FROM activite", function (err, row) {
+        if (err) {
+            res.json({ "nom": err });
         }
+        else {
+            res.json({ "nom": JSON.stringify(row) });
+        }
+    });
+
+});
+
+restapi.post('/api', function (req, res) {
+    db.run("UPDATE counts SET value = value + 1 WHERE key = ?", "counter", function (err, row) {
+        if (err) {
+            console.err(err);
+            res.status(500);
+        }
+        else {
+            res.status(202);
+        }
+        res.end();
     });
 });
 
-// more routes for our API will happen here
 
-// REGISTER OUR ROUTES -------------------------------
-// all of our routes will be prefixed with /api
-app.use('/api', router);
+restapi.listen(5213);
 
-// START THE SERVER
-// =============================================================================
-app.listen(port);
-console.log('Server started @ ' + port);
+console.log("Submit GET or POST to http://localhost:5213/api");
