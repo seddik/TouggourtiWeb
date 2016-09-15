ce01058b6f2f5012da80aa3ca45d53550965fd9b
LARABA MOHAMMED SEDDIK <lmseddik@icloud.com>
2016-09-15 12:43:17 +0100

    testing local restful

--- src/app/app.js
+++ src/app/app.js
@@ -1,8 +1,11 @@
 var express = require('express');
 var exphbs = require('express-handlebars');
 var path = require('path');
+var http = require('http');
 
 var app = express();
+var server = http.createServer(app);
+ 
 
 app.use(express.static(path.resolve(__dirname, 'www')));
 
@@ -11,9 +14,8 @@ app.set('views', path.resolve(__dirname, 'views'));
 app.set('view engine', '.html');
 
 app.get('/', function (req, res) {
-    res.render('home', { nom: 'seddik' });
+    res.render('home', { host:JSON.stringify( server.address()) });
 });
 
-app.listen(3333, function () { 
-    console.log('Touggourti app listening on port 3333');
-});
+server.listen(3333);
+console.log('Express server started on port %s', server.address().port);
